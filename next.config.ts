44fb0a6a6d6f6e159fff70a8b59cc55c971e9d6e
Oluwasegun122 <ooluwasegunismaheel@gmail.com>
2025-03-27 00:44:12 +0100

    Deploy Next.js to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/blessediog",
-  trailingSlash: true,
 };
-// add
 export default nextConfig;
