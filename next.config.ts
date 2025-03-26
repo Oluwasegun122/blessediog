b3ace5b1bb5b260fdced4953f4170f414f23cb13
Oluwasegun122 <ooluwasegunismaheel@gmail.com>
2025-03-26 22:14:14 +0100

    deply redo

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  /* config options here */
+  images: {
+    unoptimized: true, // Required for GitHub Pages
+  },
+  basePath: "/bessediog", // Replace with your GitHub repository name
+  assetPrefix: "/bessediog/", // Required for assets to load correctly
 };
 
 export default nextConfig;
