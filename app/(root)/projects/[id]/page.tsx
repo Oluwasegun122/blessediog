02cac7c84f7408f6b6ad12eb3a2bdb6f107735d0
Oluwasegun122 <ooluwasegunismaheel@gmail.com>
2025-03-27 18:39:27 +0100

    Deploy Next.js to GitHub Page

--- app/(root)/projects/[id]/page.tsx
+++ app/(root)/projects/[id]/page.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
 "use client";
+import Image from "next/image";
+
 
 import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
