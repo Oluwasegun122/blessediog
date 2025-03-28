2ec60068cd9e30345e1cb9286035f9340160a2d5
Oluwasegun122 <ooluwasegunismaheel@gmail.com>
2025-03-28 15:30:44 +0100

    Deploy Next.js to GitHub Page

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -12,10 +12,10 @@ export default function AboutMe() {
             alt="Profile Picture"
             width={150}
             height={150}
-            className="rounded-t-full"
+            className="rounded-t-full object-cover"
           />
         </div>
-        <div className="bg-blue-500 text-white px-4 py-2 mt-[-10px] rounded-b-lg shadow-md">
+        <div className="bg-blue-500 text-white px-4 py-2 -mt-2 rounded-b-lg shadow-md">
           <h2 className="text-lg font-bold">Faith Dasola Obajafa</h2>
         </div>
       </div>
@@ -23,14 +23,14 @@ export default function AboutMe() {
       {/* Text Section */}
       <div className="text-center md:text-left max-w-lg">
         <p className="text-gray-700 text-lg mb-4">
-          Hi, I'm Faith Obajafa, a detail-driven data analyst with a strong
+          Hi, I&apos;m Faith Obajafa, a detail-driven data analyst with a strong
           passion for uncovering hidden trends and insights. With 5 years of
           experience in data analysis, I excel at transforming complex data into
           actionable recommendations that drive business growth.
         </p>
         <Link
           href="/about_me"
-          className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-600"
+          className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-600 transition-all"
         >
           Read More
         </Link>
