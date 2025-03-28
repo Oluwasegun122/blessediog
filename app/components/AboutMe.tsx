6a1f05e5e5c02e94e9b65fc1c07fbe9489b731e6
Oluwasegun122 <ooluwasegunismaheel@gmail.com>
2025-03-28 15:04:22 +0100

    Deploy Next.js to GitHub Page

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -7,32 +7,41 @@ export default function AboutMe() {
       <div className="flex justify-center mb-4">
         <div className="avatar md:hidden flex-col items-center">
           <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
-            <Image src="./images/img.jpg" alt="" />
+            {/* ✅ Corrected Image Path & Added width/height */}
+            <Image
+              src="/images/img.jpg"
+              alt="Profile Picture"
+              width={100}
+              height={100}
+            />
           </div>
           <h1 className="text-2xl md:text-5xl font-bold">
-            I'm Faith Dasola Obajafa
+            I&apos;m Faith Dasola Obajafa
           </h1>
         </div>
       </div>
-      <div className=" hero bg-base-200 rounded-lg">
+      <div className="hero bg-base-200 rounded-lg">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="ring-2 ring-blue-700 w-3xl rounded-t-full p-2">
+            {/* ✅ Corrected Image Path & Added width/height */}
             <Image
-              src="./images/img.jpg"
-              alt=""
+              src="/images/img.jpg"
+              alt="Profile Picture"
+              width={300}
+              height={300}
               className="hidden md:block rounded-t-full pb-1"
             />
             <div className="ring-0 bg-blue-500 flex justify-center items-center p-2">
-              {" "}
               <h1 className="text-white font-bold">Obajafa Faith Dasola</h1>
             </div>
           </div>
           <div>
             <p className="py-6 text-2xl">
-              Hi, I'm Faith Obajafa, a detail-driven data analyst with a strong
-              passion for uncovering hidden trends and insights. With 5 years of
-              experience in data analysis, I excel at transforming complex data
-              into actionable recommendations that drive business growth.
+              Hi, I&apos;m Faith Obajafa, a detail-driven data analyst with a
+              strong passion for uncovering hidden trends and insights. With 5
+              years of experience in data analysis, I excel at transforming
+              complex data into actionable recommendations that drive business
+              growth.
             </p>
             <Link href="/about_me" className="btn btn-primary">
               Read More
