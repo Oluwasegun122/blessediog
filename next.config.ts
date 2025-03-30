c4317430d227829741974e629e1d6d52a138d774
Oluwasegun122 <ooluwasegunismaheel@gmail.com>
2025-03-30 01:44:47 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,10 @@
 const nextConfig = {
   output: "export",
 };
-<<<<<<< HEAD
 module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
-=======
->>>>>>> 01ae528b09dc9ea5701947b5e5d442c0c838d035
 // add
 export default nextConfig;
