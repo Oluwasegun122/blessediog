0e60ab5d6d4360197c911179ada4c929a530394c
Oluwasegun122 <ooluwasegunismaheel@gmail.com>
2025-03-30 07:22:31 +0100

    first commit

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -3,9 +3,27 @@ import Link from "next/link";
 
 export default function AboutMe() {
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center bg-white p-6 rounded-lg shadow-lg w-full md:w-3/4 mx-60">
+    <div className="flex flex-col md:flex-row justify-center items-center bg-white p-6 rounded-lg shadow-lg w-full max-w-3xl mx-auto">
+      {/* Image Section */}
+      <div className="relative flex flex-col items-center mb-6 md:mb-0 md:mr-6">
+        <div className="w-40 h-40 ring-4 ring-blue-500 rounded-full overflow-hidden">
+          <Image
+            src="/images/img.jpg"
+            alt="Profile Picture"
+            width={160}
+            height={160}
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="bg-blue-500 text-white px-4 py-2 -mt-3 rounded-b-lg shadow-md">
+          <h2 className="text-lg font-bold text-center">
+            Faith Dasola Obajafa
+          </h2>
+        </div>
+      </div>
+
       {/* Text Section */}
-      <div className="text-center items-center md:text-left max-w-lg">
+      <div className="text-center md:text-left max-w-lg">
         <p className="text-gray-700 text-lg mb-4">
           Hi, I&apos;m Faith Obajafa, a detail-driven data analyst with a strong
           passion for uncovering hidden trends and insights. With 5 years of
@@ -18,21 +36,6 @@ export default function AboutMe() {
         >
           Read More
         </Link>
-      </div>
-      {/* Image Section */}
-      <div className="relative flex flex-col items-center mb-6 md:mb-0 md:mr-6">
-        <div className="ring-4 ring-blue-500 rounded-t-full overflow-hidden">
-          <Image
-            src="/images/img.jpg"
-            alt="Profile Picture"
-            width={150}
-            height={150}
-            className="rounded-t-full object-cover"
-          />
-        </div>
-        <div className="bg-blue-500 text-white px-4 py-2 -mt-2 rounded-b-lg shadow-md">
-          <h2 className="text-lg font-bold">Faith Dasola Obajafa</h2>
-        </div>
       </div>
     </div>
   );
