fbb2a6d8f5585e93331cc35ca05d1878f9ae851e
Oluwasegun122 <ooluwasegunismaheel@gmail.com>
2025-03-30 09:47:30 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-200`}
       >
         <Header />
-        <div className="mt-20">{children}</div>
+        {children}
       </body>
     </html>
   );
